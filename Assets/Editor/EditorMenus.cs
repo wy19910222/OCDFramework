f0c99bdbf0ddd1062ee28316c40eb54243570915
yun.wang <wy19910222@hotmail.com>
2023-03-26 03:31:36 +0800

    EditorMenus

---
+++ Assets/Editor/EditorMenus.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * @Author: wangyun
+ * @CreateTime: 2022-07-01 12:49:02 083
+ * @LastEditor: wangyun
+ * @EditTime: 2022-07-01 12:49:02 076
+ */
+
+using UnityEditor;
+using UnityEngine;
+
+public class EditorMenus : Editor {
+	[MenuItem("GameObject/批处理（要怎么处理自己临时改代码）", priority = -1)]
+	protected static void BatchOperation(MenuCommand command) {
+		GameObject go = command.context as GameObject;
+		Debug.Log(go, go);
+		// TODO: 对单个对象处理
+	}
+}
